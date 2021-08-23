4bb139b518e34688a6783e779e321759eb75d09b
Cory <46043575+hclicory@users.noreply.github.com>
2021-08-23 15:28:09 +0800

    Add isInteger checking

--- IEEE754Convertor.java
+++ IEEE754Convertor.java
@@ -2,6 +2,10 @@ public class IEEE754Convertor {
 	private static String sign;
 	private static String exponent;
 	private static String mantissa;
+	private static String mantissa1;
+	private static String mantissa2;
+	private static String mantissa3;
+
 	private static String outSign;
 	private static double outMantissa;
 	private static int outExponent;
@@ -25,6 +29,19 @@ public class IEEE754Convertor {
 		}
 	}
 
+	public static boolean checkInteger(){
+		if (	isInteger(sign) &&
+			isInteger(exponent) &&
+			isInteger(mantissa1) &&
+			isInteger(mantissa2) &&
+			isInteger(mantissa3)
+		){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
 	public static boolean isInteger(String text){
 		try
 		{
@@ -34,6 +51,32 @@ public class IEEE754Convertor {
 		catch (NumberFormatException e)
 		{
 			return false;
+		}
+	}
+
+	public static boolean checkBinary(){
+		if (	isBinary(Integer.parseInt(sign)) &&
+			isBinary(Integer.parseInt(exponent)) &&
+			isBinary(Integer.parseInt(mantissa1)) &&
+			isBinary(Integer.parseInt(mantissa2)) &&
+			isBinary(Integer.parseInt(mantissa3))
+		){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	public static boolean isBinary(int binary){
+		while(true){
+			if (binary == 0) {
+				return true;
+			}else{
+				if (binary%10 > 1) {
+					return false;
+				}
+				binary = binary/10;
+			}
 		}
 	}
 
@@ -84,36 +127,46 @@ public class IEEE754Convertor {
 				exponent = exponent.substring(0,8);
 				mantissa = mantissa.substring(0,23);
 
-				System.out.println("---------------------------");
-				System.out.println("Binary value is: " + sign + " " + exponent + " " + mantissa);
-				System.out.println("---------------------------");
+				//Split large number
+				mantissa1 = mantissa.substring(0,8);
+				mantissa2 = mantissa.substring(8,16);
+				mantissa3 = mantissa.substring(16,23);
 
-				if (isInteger(sign) && isInteger(exponent) && isInteger(mantissa.substring(0,8)) && isInteger(mantissa.substring(8,16)) && isInteger(mantissa.substring(16,23))){
-					if (sign.equals("0")) {
-						outSign = "+";
+				if (checkBinary()){
+
+					System.out.println("---------------------------");
+					System.out.println("Binary value is: " + sign + " " + exponent + " " + mantissa);
+					System.out.println("---------------------------");
+
+					if (checkInteger()){
+						if (sign.equals("0")) {
+							outSign = "+";
+						}else{
+							outSign = "-";
+						}
+						System.out.println("Sign is : " + outSign);
+
+						outExponent = getDecimal(Integer.parseInt(exponent));
+						System.out.println("Exponent is : " + outExponent + " (" + outExponent + " - 127 = " + (outExponent - 127) + " )");
+
+						outMantissa = getFloatingPoint(Integer.parseInt(mantissa1), 8, 0);
+						outMantissa = outMantissa + getFloatingPoint(Integer.parseInt(mantissa2), 16, 8);
+						outMantissa = outMantissa + getFloatingPoint(Integer.parseInt(mantissa3), 23, 16);
+						outMantissa = 1 + outMantissa;
+						System.out.println("Mantissa is : " + outMantissa);
+
+						System.out.println("---------------------------");
+						resultNoSide = outMantissa * Math.pow(2, outExponent-127);
+						System.out.println(outMantissa + " * 2^" + (outExponent-127) + " = " + resultNoSide);
+
+						System.out.println("---------------------------");
+						System.out.println("The Decimal value is : " + outSign + resultNoSide);
+
 					}else{
-						outSign = "-";
+						System.out.println("You must input integer only.");
 					}
-					System.out.println("Sign is : " + outSign);
-
-					outExponent = getDecimal(Integer.parseInt(exponent));
-					System.out.println("Exponent is : " + outExponent + " (" + outExponent + " - 127 = " + (outExponent - 127) + " )");
-
-					outMantissa = getFloatingPoint(Integer.parseInt(mantissa.substring(0,8)), 8, 0);
-					outMantissa = outMantissa + getFloatingPoint(Integer.parseInt(mantissa.substring(8,16)), 16, 8);
-					outMantissa = outMantissa + getFloatingPoint(Integer.parseInt(mantissa.substring(16,23)), 23, 16);
-					outMantissa = 1 + outMantissa;
-					System.out.println("Mantissa is : " + outMantissa);
-
-					System.out.println("---------------------------");
-					resultNoSide = outMantissa * Math.pow(2, outExponent-127);
-					System.out.println(outMantissa + " * 2^" + (outExponent-127) + " = " + resultNoSide);
-
-					System.out.println("---------------------------");
-					System.out.println("The Decimal value is : " + outSign + resultNoSide);
-
 				}else{
-					System.out.println("You must input integer only.");
+					System.out.println("You can enter 0 or 1 only");
 				}
 			}else{
 				System.out.println("You must input 3 number");
